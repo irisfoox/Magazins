c4c3249a98429f7a7d0280a336baf9ed175c22d8
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2021-05-10 15:10:55 +0300

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import PagesBar from './Components/PagesBar'
+
+function App() {
+  return (
+    <div className="App">
+      <PagesBar/>
+    </div>
+  );
+}
+
+export default App;
