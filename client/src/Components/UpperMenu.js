25417a54182497860d1e7b40533cf491bc92d4cc
irisfoox <fooxit99@gmail.com>
2021-05-25 19:05:37 +0300

    template & homepage

--- client/src/Components/UpperMenu.js
+++ client/src/Components/UpperMenu.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import '../App.css'
+import leader from '../pic/leader.png'
+import person from '../pic/person.png'
+import pin from '../pic/pin.png'
+import line from '../pic/line.png'
+import Home from './Home'
+
 
 export default function UpperMenu() {
     return (
-        <div className="row" style={{width:"100%",height:"8%"}}>
+        <div className="container">
+        <div className="row d-flex" style={{width:"100%",height:"8%",background: "var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box",
+          backgroundColor: "#FFFFFF 0% 0% no-repeat padding-box",boxShadow:"0px 3px 6px #00000029",opacity: "1"
+          }}>
             <div className="col-1">
-                <img src="leader" alt="logo" height="38px"></img>
+                <img src={leader} alt="logo" height="20px"></img>
             </div>
-            <div className="col-10"></div>
-            <div className="col-1">
-                <img src="person" alt="user" height="38px"></img>
-                <img src="pin" alt="thumbtack" height="38px"></img>
-                <img src="line" alt="settings" height="38px"></img>
+            <div className="col-8"></div>
+            <div className="col-2 pt-2 d-flex justify-content-right">
+                <img src={person} alt="user" height="20px"></img>
+                <img src={pin} alt="thumbtack" height="20px"></img>
+                <img src={line} alt="settings" height="2px" width="20px"></img>              
             </div>
         </div>
+        <div className="row d-inline-flex" style={{width:"100%",height:"40%"}}>
+                 <div className="side col-1"></div>
+                 <div className="col-11" style={{backgroundColor: "#E8EAEC"}}>
+                     <Home/>
+                 </div>
+        </div>
+    </div>
     )
 }
