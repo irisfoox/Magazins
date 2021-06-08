dec7768ba2b1a432840d8d75d9c1da198016964c
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2021-06-08 14:50:55 +0300

    Add files via upload

---
+++ client/src/Components/NewPost.js
@@ -0,0 +1,83 @@
+import React, { useRef,useState,useEffect  } from 'react';
+//import {connect} from 'react-redux';
+import { useParams, Redirect,Link  } from 'react-router-dom';
+//import Modal from 'react-awesome-modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Style from '../Style.css';
+import adjust from '../pic/adjust.png'
+export default function createPost(){
+
+
+    return(
+        <>  
+        <div className="container-fluid">
+              <div className="col-10">
+                <div>
+                <div class="d-flex justify-content-between">
+                    <Link to='/Home'>&lt;Back Home</Link>
+                    <Link to='/Set'>To The Magazin &gt;</Link>
+                    </div>
+        </div>
+
+      
+
+<div className="body">
+        <div className="container-fluid">
+        <div className="col-10 logo mb-3">
+           <div className="pic"> 
+           <div className="d-flex align-items-end flex-column" >
+           {/* <div type="button" className="btn btn-primary mt-auto p-2">category</div> */}<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+           <div className="mt-auto p-2"><button className="b1"><b>Category</b></button></div>
+           {/* btn btn-primary b1 */}
+           </div>
+           </div>
+        </div>
+             
+        
+<div className="white m-10" Style="padding-left: 5%;">
+      <div className="row">
+          <div className="col-8 p-0 m-2">
+          <div> <p className="text-justify label1"><b>Type your post title</b></p></div>
+    </div>
+    </div>
+
+    <div className="row">
+          <div className="col-8 p-0 ">
+          <div> <p className="text-justify"><b>Write stories about your business and use them to engage your customers. Your Stories Blog can be shared on your website.</b></ p></div>
+    </div>
+    </div>
+<br/>
+    <div className="row">
+          <div className="col-8 p-0 ">
+          <div> <p className="text-justify">Write stories about your business and use them to engage your customers. Your Stories Blog can be shared on your website.Write stories about your business and use them to engage your customers. Your Stories Blog can be shared on your website.Write stories about your business and use them to engage your customers. Your Stories Blog can be shared on your website.</ p>
+          <p className="text-justify"><b>________</b></p>
+          </div>
+    </div>
+    </div>
+    <div className="row">
+          <div className="col-2 p-0 ">
+        <p className="text-justify">By Mattie Bryan</p>
+          </div></div>
+</div>
+</div>
+</div>
+
+<div className="row">
+<div className="col-3 ss">
+
+ {/* d-flex flex-row-reverse */}
+ <br/><br/>
+ {/*  className="img1"*/}
+ <div>  <img  src={adjust} alt="adjust"></img></div>
+        <div>Create New Page</div>
+      </div>
+      </div>
+     
+
+    
+      </div>
+    </div>     
+    
+        </>
+        )
+} 
