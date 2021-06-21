997625945adb3a34c55d2a0b1c0ea6dce8fd423d
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2021-06-21 17:49:31 +0300

    Add files via upload

--- client/Components/UpperMenu.js
+++ client/Components/UpperMenu.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import '../App.css'
+//import '../style.css'
 import leader from '../pic/leader.png'
 import person from '../pic/person.png'
 import pin from '../pic/pin.png'
-//import line from '../pic/line.png'
+import adjust from '../pic/adjust.png'
 import Home from './Home'
-//<img src={line} alt="settings" height="2px" width="20px"></img>
+
 
 export default function UpperMenu() {
     return (
         <div className="container">
-        <div className="row d-flex" style={{width:"100%",height:"5%",background: "var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box",
-          backgroundColor: "#FFFFFF 0% 0% no-repeat padding-box",boxShadow:"0px 3px 6px #00000029",opacity: "1"
+        <div className="row d-flex" style={{width:"100%",height:"5%",
+          background: "#ffffff 0% 0% no-repeat padding-box",boxShadow:"0px 3px 6px #00000029",opacity: "1"
           }}>
             <div className="col-1">
                 <img src={leader} alt="logo" height="20px"></img>
@@ -25,10 +26,18 @@ export default function UpperMenu() {
         </div>
         <div className="row d-inline-flex" style={{width:"100%",height:"40%"}}>
                  <div className="side col-1"></div>
-                 <div className="col-11" style={{backgroundColor: "#E8EAEC"}}>
+                 <div className="col-9" style={{backgroundColor: "#E8EAEC"}}>
                      <Home/>
                  </div>
-        </div>
-    </div>
+                 <div className="col-2 ss">
+
+                  {/* d-flex flex-row-reverse */}
+                   <br/><br/>
+                  {/*  className="img1"*/}
+                  <div> <img  src={adjust} alt="adjust"></img></div>
+                  <div>Create New Page</div>
+                  </div>
+                  </div>
+                  </div>
     )
 }
