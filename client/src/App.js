eb9a18fe2b946f0742e8d29f09808e878f30878f
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2021-06-29 13:41:49 +0300

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,13 @@ import React from 'react'
 import './App.css';
 import PagesBar from './Components/PagesBar'
 import UpperMenu from './Components/UpperMenu'
-//import NewPost from './Components/NewPost'
-/*import {BrowserRouter as Router,
-  Switch,Route} from 'react-router-dom'*/
-
-  /*<Router>
-  <div><Route path="/UpperMenu">
-  <UpperMenu/>
-  </Route>
-  <Switch>
-    <Route path="/PagesBar">
-    <PagesBar/>
-    </Route>
-    <Route path="/NewPost">
-    <NewPost/>
-    </Route>
-</Switch>
-</div> 
-</Router>*/
 
 function App() {
   return (
     <div>
       <UpperMenu/>
-      <PagesBar/>
+      
+    <PagesBar/>
     </div>
   );
 } 
