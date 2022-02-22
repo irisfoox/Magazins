85f318c7cf4672f29cb0f454c7563e9747397317
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2022-02-22 14:13:14 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,29 @@
-const express=require('express')
-const app=express();
-const router=require('./route/api');
-app.listen(8080,()=>
-{
-    console.log('listening')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const router = require('./routes/api');
+
+const ConectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 }
-);
-// app.use('/',route)
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB, ConectionParams)
+    .then(() => {
+        console.log("DB connected");
+    }).catch((err) => {
+        console.log("error: \n" + err);
+    })
+app.use(bodyParser.json())
+app.use('/', router)
+
+
+app.listen(3000, () => {
+    console.log("listening 3000");
+})
