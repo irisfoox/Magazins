1aa8b497609e84c7332f632bf71ec9f8cc9230c7
Iris Foox <57198497+irisfoox@users.noreply.github.com>
2022-02-22 14:37:53 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -27,3 +27,39 @@ app.use('/', router)
 app.listen(3000, () => {
     console.log("listening 3000");
 })
+
+/*const express=require('express')
+const app=express();
+const router=require('./routes/api');  
+const bodyParser=require('bodyparser');
+
+app.get('/details',(req,res)=>{
+    console.log('details')
+    console.log(req.query.age,req.query.height)
+    age:req.query.age;
+    height:req.query.height;
+})
+app.post('/kinds',(req,res)=>{
+    res.send('success');   //sends text as is
+    res.json({"status":"success"})  //sends as object in body
+})
+app.get('/jwt',(req,res)=>{
+    console.log(req.params.name);
+    console.log('secret protected',process.env.MONGO_DB);
+    const token=jwt.sign({name:req.params.name},process.env.MONGO_DB);
+    res.status(200).json({message:'create token',token:token})
+})
+app.get('/verifyJwt',(req,res)=>{
+    console.log(req.headers['authorization']);
+    const token=jwt.verify(req.headers['authorization']); //must lowercase
+    console.log(token);
+    res.status(200).
+
+})
+
+app.listen(3000,()=>
+{
+    console.log('listening port 3000')
+}
+);
+// app.use('/',route)    */
